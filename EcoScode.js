3bc74913ff51241abda3ee472bd90c6bf4ebfba8
cfossto <70951673+cfossto@users.noreply.github.com>
2020-12-28 12:16:02 +0100

    First commit

---
+++ EcoScode.js
@@ -0,0 +1,22 @@
+
+
+let obj = [
+    
+    { id: 50,
+        productName: "Jobba",
+        price: 50,
+        hours: 40,
+ }
+]
+
+
+
+for (let i of obj ){
+
+    totalsum = i.price*i.hours
+
+    console.log(totalsum)
+
+    
+
+}
